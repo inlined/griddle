60586b457c75a4b844e0c9a75b71fdf21febdd76
Thomas Bouldin <thomas@parse.com>
2015-06-05 12:36:46 -0700

    Initial Commit

---
+++ Griddle/Griddle.h
@@ -0,0 +1,19 @@
+//
+//  Griddle.h
+//  Griddle
+//
+//  Created by Thomas Bouldin on 6/5/15.
+//  Copyright (c) 2015 Inlined. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Griddle.
+FOUNDATION_EXPORT double GriddleVersionNumber;
+
+//! Project version string for Griddle.
+FOUNDATION_EXPORT const unsigned char GriddleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Griddle/PublicHeader.h>
+
+
